224ac6df054a1399055d14298f61ed1737977841
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2025-12-10 23:03:33 +0530

    updated requirements

--- app/app.py
+++ app/app.py
@@ -2,12 +2,13 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 from dotenv import load_dotenv
 
-
+k = input("enter the prompt : ")
 load_dotenv()
 
 
 model = ChatGoogleGenerativeAI(model = "gemma-3-12b-it")
-prompt = "what is capital of india"
+prompt = "image your name is simal and give response for boy abhishek like you are mentor of abhishek so answer his questions : "
+prompt = prompt + "\n" + k
 result = model.invoke(prompt)
 
 
