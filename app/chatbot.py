2d4ccd39cc8bb0536a3b7e58be298b6f88bbb740
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2025-12-10 23:20:36 +0530

    build a memory based agent

---
+++ app/chatbot.py
@@ -0,0 +1,25 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+models = ChatGoogleGenerativeAI(model = "gemma-3-12b-it")
+
+chat_history = [
+    SystemMessage(content = "your name is abhishek b shetty and you are helpfull ai agent")
+
+
+]
+
+
+while True:
+    a = input("You : ")
+    chat_history.append(HumanMessage(content = a))
+    if(a == "exit"):
+        break
+    result = models.invoke(str(chat_history))
+    chat_history.append(AIMessage(result.content))
+    print("Bot : ",result.content)
