63fde0c693889d3b7cadbde70bcd6885e327dff0
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2025-12-11 23:29:39 +0530

    memory and role based chat using ollama

--- app/test.py
+++ app/test.py
@@ -1,11 +1,17 @@
 from langchain_ollama import ChatOllama
+from lanchain_core import SystemMessage, HumanMessage, AIMessage
 
 
-model = ChatOllama(model = "gemma3:1b")
+model = ChatOllama()
 
 
+chat_history = [SystemMessage(content = "You are an AI mentor for student abhishek help with his query and be like mentor")]
 
-p = model.invoke("hii")
-print(p.content)
+while True:
+    n = input("USER : ")
+    chat_history.append(HumanMessage(n))
+    out = model.invoke(chat_history)
+    chat_history.append(AIMessage(out))
+    print("AI : ",out)
 
 
