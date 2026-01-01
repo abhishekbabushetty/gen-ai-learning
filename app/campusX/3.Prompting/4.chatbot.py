c2b878b4df5d05c2f69fda388044951dc9f0fd53
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2026-01-01 18:35:13 +0530

    solve it

--- app/campusX/3.Prompting/4.chatbot.py
+++ app/campusX/3.Prompting/4.chatbot.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-chat_history = [SystemMessage("you are an ai mentor today keep messages conversation very short")]
+chat_history = [SystemMessage(content = "you are an ai mentor today keep messages conversation very short")]
 
 
 
@@ -18,13 +18,13 @@ model = ChatOllama(model="gemma3:1b")
 while(True):
 
     a = input("Human : ")
-    chat_history.append(HumanMessage(a))
+    chat_history.append(HumanMessage(content = a))
     if a == "exit":
         break
 
     response = model.invoke(chat_history)
 
-    chat_history.append(AIMessage(response.content))
+    chat_history.append(AIMessage(content = response.content))
   
     print("AI :",response.content)
 
